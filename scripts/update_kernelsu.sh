ffb34fb68913140c742bea8f58a30acd99032c84
DJLeleX <29524391+DJLeleX@users.noreply.github.com>
2025-12-16 17:20:00 +0100

    Switch to sidex15 KernelSU and susfs branch
    See: https://github.com/sidex15/KernelSU/tree/susfs-rksu-master

--- scripts/update_kernelsu.sh
+++ scripts/update_kernelsu.sh
@@ -4,9 +4,10 @@ exec 9>.kernelsu-fetch-lock
 flock -n 9 || exit 0
 [[ $(( $(date +%s) - $(stat -c %Y "drivers/kernelsu/.check" 2>/dev/null || echo 0) )) -gt 86400 ]] || exit 0
 
-AUTHOR="rsuntk"
+AUTHOR="sidex15"
 REPO="KernelSU"
-VERSION=`curl -s -I -k "https://api.github.com/repos/$AUTHOR/$REPO/commits?per_page=1" | sed -n '/^[Ll]ink:/ s/.*"next".*page=\([0-9]*\).*"last".*/\1/p'`
+BRANCH="susfs-rksu-master"
+VERSION=`curl -s -I -k "https://api.github.com/repos/$AUTHOR/$REPO/commits?sha=$BRANCH&per_page=1" | sed -n '/^[Ll]ink:/ s/.*"next".*page=\([0-9]*\).*"last".*/\1/p'`
 #LATEST_RELEASE=$(curl -s -k "https://api.github.com/repos/$AUTHOR/$REPO/releases/latest" | grep -oP '"tag_name": "\K[^"]+')
 #VERSION=`curl -s -I -k "https://api.github.com/repos/$AUTHOR/$REPO/commits?per_page=1&sha=$LATEST_RELEASE" | sed -n '/^[Ll]ink:/ s/.*"next".*page=\([0-9]*\).*"last".*/\1/p'`
 
@@ -15,11 +16,11 @@ if [[ -f drivers/kernelsu/.version && *$(cat drivers/kernelsu/.version)* == *$VE
 	exit 0
 fi
 
-# printf "$REPO updating to $((10000+$VERSION+200))\n"
+# printf "$REPO updating to $((30000+$VERSION+200))\n"
 rm -rf drivers/kernelsu
 mkdir -p drivers/kernelsu
 cd drivers/kernelsu
-wget -q -O - https://github.com/$AUTHOR/$REPO/archive/refs/heads/main.tar.gz | tar -xz --strip=2 "$REPO-main/kernel"
+wget -q -O - https://github.com/$AUTHOR/$REPO/archive/refs/heads/$BRANCH.tar.gz | tar -xz --strip=2 "$REPO-$BRANCH/kernel"
 #wget -q -O - "https://github.com/$AUTHOR/$REPO/archive/refs/tags/$LATEST_RELEASE.tar.gz" | tar -xz --strip=2 "$REPO-${LATEST_RELEASE#v}/kernel"
 echo $VERSION >> .version
 touch .check
