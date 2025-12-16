b6fa74c0fd5c79c9031a5638ea3c1e876949b368
DJLeleX <29524391+DJLeleX@users.noreply.github.com>
2025-12-16 17:21:10 +0100

    fs/kernel: update manual hooks to v1.6
    See: https://github.com/sidex15/android_kernel_lge_sm8150/commit/87e6c1fcbd9c007b6a9e34aafdbebc86fd6590f6

--- kernel/reboot.c
+++ kernel/reboot.c
@@ -299,6 +299,7 @@ static DEFINE_MUTEX(reboot_mutex);
 #ifdef CONFIG_KSU
 extern int ksu_handle_sys_reboot(int magic1, int magic2, unsigned int cmd, void __user **arg);
 #endif
+
 SYSCALL_DEFINE4(reboot, int, magic1, int, magic2, unsigned int, cmd,
 		void __user *, arg)
 {
